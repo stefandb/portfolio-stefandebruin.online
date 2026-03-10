fdc795f8b43ca022b693bb4648165f6ee8a9deb8
Stefan de Bruin <stefan@procademy.nl>
2026-03-10 18:39:30 +0100

    install spatie laravel-medialibrary and laravel-tags

---
+++ database/migrations/2026_03_08_200248_create_projects_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('slug')->unique();
+            $table->text('excerpt');
+            $table->longText('content');
+            $table->string('github_url')->nullable();
+            $table->string('demo_url')->nullable();
+            $table->string('company')->nullable();
+            $table->string('role')->nullable();
+            $table->year('year');
+            $table->string('status');
+            $table->timestamp('published_at')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('projects');
+    }
+};
